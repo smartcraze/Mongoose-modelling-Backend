a367790dfacdd3d2420c6f5b4e300a96e5508042
smartcraze <smartcraze354@gmail.com>
2024-03-01 00:08:54 +0530

    adding schema

---
+++ Mongoos Basic Modelling/Lms/registration.models.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+ const registrationSchema = new mongoose.Schema({},{timestamps:true});
+export const Registration = mongoose.model("Registration",registrationSchema);
