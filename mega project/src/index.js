164ea214bb57dfcdadac1fbb304476f5b5fda7eb
smartcraze <smartcraze>
2024-03-03 10:27:06 +0530

    finally connected

--- mega project/src/index.js
+++ mega project/src/index.js
@@ -1,57 +1,11 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-
-
-
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
 dotenv.config({
-    path: './env'
-});
+    path: './.env'
+})
+
+
 
 connectDB()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const app = express();
-
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        
-        mongoose.connection.on("error", (error) => {
-            console.error("MongoDB Connection failed ho gya :", error);
-            process.exit(1);
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.error("Error connecting to the database:", error);
-        process.exit(1);
-    }
-})();
-*/
